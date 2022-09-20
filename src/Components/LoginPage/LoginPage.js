7df3d06536d0f5df762bb846afe2a4d3ff47ece0
Elie Soued <eliegebransoued@gmail.com>
2022-09-20 20:53:24 +0300

    creating a login page

---
+++ src/Components/LoginPage/LoginPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+export default function LoginPage() {
+  return (
+    <>
+      <div className="pilou">
+        <h1 className="pilou2">hello world</h1>
+      </div>
+    </>
+  );
+}
