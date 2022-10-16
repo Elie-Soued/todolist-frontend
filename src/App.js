5f65cace271b009578cf5a5397cbb6598f7edf9f
Elie Soued <eliegebransoued@gmail.com>
2022-10-16 21:21:40 +0300

    remove dead code

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import LoginPage from "./Components/LoginPage/LoginPage";
 import { Route, Routes, HashRouter } from "react-router-dom";
 
 export default function App() {
-  const token = localStorage.getItem("token");
-  console.log("token :>> ", token);
-
   return (
     <HashRouter>
       <Routes>
